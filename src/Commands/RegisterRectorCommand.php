8efe3d471b8ebbf7744ebb73b45398c158302517
Bruno Henrique da Costa <bruno.costa@mobiis.com.br>
2025-05-08 11:53:44 -0300

    feat: add RegisterRectorCommand and rector configuration

---
+++ src/Commands/RegisterRectorCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Costa\Ready\Commands;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+
+use function Laravel\Prompts\text;
+
+final class RegisterRectorCommand extends Command
+{
+    protected $signature = 'ready:register-stan';
+
+    protected $description = 'Install Rector for your application';
+
+    public function handle(): void
+    {
+        $this->info('Installing Rector...');
+
+        if (verifyInstallDependency('rector/rector')) {
+            executeCommand('composer require rector/rector --dev');
+        }
+
+        $endpoint = text(
+            'What address is your php rector configuration?',
+            default: 'https://raw.githubusercontent.com/bhcosta90/laravel-ready/main/fixtures/rector.php'
+        );
+
+        $content = (new Client())->get($endpoint);
+
+        file_put_contents(
+            base_path('rector.php'),
+            $content->getBody()->getContents()
+        );
+
+        $this->info('Rector already installed');
+    }
+}
